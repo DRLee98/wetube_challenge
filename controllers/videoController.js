b286aaaabe645b4bdf16f0169086b6468e206617
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2020-09-28 21:28:24 +0900

    mongo schema

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,30 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const editVideo = (req, res) => res.send("Edit Video");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
+};
+
+export const getUpload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description },
+  } = req;
+  // To Do: Upload and save video
+  res.redirect(routes.videoDetail(324393));
+};
+
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "Video Detail" });
+
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "Edit Video" });
+
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
