c7c6dbb3c109e52a029bd959546205bac8e11047
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2020-10-21 23:00:45 +0900

    stress

--- src/db.js
+++ src/db.js
@@ -5,8 +5,7 @@ dotenv.config();
 
 mongoose.connect(process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL, {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
 
 const db = mongoose.connection;
