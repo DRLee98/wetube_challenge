56a908e11b37fe7c3cf64b591925b47000da7aeb
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2020-10-21 22:56:26 +0900

    OMG

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL_PROD, {
+mongoose.connect(process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
