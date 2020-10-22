144a8f339dc6c541f3184e0b829631b42ccf604d
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2020-10-22 20:18:22 +0900

    content-security-policy

--- src/passport.js
+++ src/passport.js
@@ -12,7 +12,9 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`,
+      callbackURL: process.env.PRODUCTION
+        ? `https://lit-woodland-85021.herokuapp.com/${routes.githubCallback}`
+        : `http://localhost:4000${routes.githubCallback}`
     },
     githubLoginCallback
   )
